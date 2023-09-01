4bbb1ee9d48bc7eb6cd00a423e5b2cfbc5b0333d
mixedfeelings13 <fuegodjh@gmail.com>
2023-09-01 03:55:17 +0100

    Creating Portfolio base

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,11 @@
 import '@/styles/globals.css'
+import { Montserrat } from "next/font/google"
+
+const montserrat = Montserrat({ 
+  subsets: ['latin'],
+  variable: "--font-mont"
+})
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
